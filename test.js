bac5284a4273d090dbcf488ca01885b267606326
Robbert van Staveren <rubstar@rubstar.nl>
2016-04-28 20:16:58 +0200

    CODACY TEST

--- test.js
+++ test.js
@@ -1,5 +1,21 @@
 var t = "hello";
 
-if (t === "hello') {
-    console.log('ok');
+if (t == "hello') {
+  console.log('ok');
+}
+
+if(true) {
+    let isthisfalse = null;
+}
+
+var thisiswrong = 'variable'
+
+if (true) {
+	if(true) {
+		if(true) {
+			if(true) {
+			  console.log('false')
+			}
+		}
+	}
 }
